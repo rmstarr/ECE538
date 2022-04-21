c84a4ad898dc6467a6193a2a942d7091b1355276
Robbie Starr <42650446+rmstarr@users.noreply.github.com>
2022-04-20 22:44:45 -0400

    Update randomJammer.m

--- PROJECT/HEdlMUex/randomJammer.m
+++ PROJECT/HEdlMUex/randomJammer.m
@@ -1,13 +1,18 @@
-% Add Comments Later
+function [jammerOut] = randomJammer(dataLength, axCfg, idleTime, header, dutyCycle)
+jamVals = 0:2;
+probs = [(1-dutyCycle) (dutyCycle/2) (dutyCycle/2)];
+selectOne = 1;
+rng = randsample(jamVals, selectOne, true, probs);
 
+switch rng
+    case 0
+        jammerOut = zeros(1, dataLength);
+    case 1
+        jammerOut = constantJammer(dataLength, axCfg, idleTime);
+    case 2
+        jammerOut = deceptiveJammer(dataLength, axCfg, idleTime, header);
+    otherwise
+        disp('Error in return value')
+end 
 
-function [jammerOut] = randomJammer(dataLength, axCfg, idleTime, header, numPackets)
-
-packets = [1:numPackets]; 
-packetsToJam = sort(randsample(packets, floor(0.3*10)));
-if packets
-    jammerSig = [header; randi([0 1], dataLength, 1, 'int8')];
-    jammerOut = wlanWaveformGenerator(jammerSig, axCfg, 'IdleTime', idleTime*1e-6);
-else
-    jammerOut = zeros(dataLength); 
 end
